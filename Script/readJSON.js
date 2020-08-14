e4da8ea9db73e8b207e5688af3820f0c6c2340eb
Emanuele Concas <conema@outlook.com>
2020-08-14 16:48:42 +0200

    "change color to" rules

--- Script/readJSON.js
+++ Script/readJSON.js
@@ -12,6 +12,10 @@ function loadJSON(path) {
     });
 }
 
+function removeSpaces(string) {
+    return string.replace(/ /g, '')
+}
+
 loadJSON('/Rules/rules.json').then(function (response) {
     var rules = JSON.parse(response);
     document.getElementById(rules[0]["object"]).setAttribute("cursor-listener", "");
@@ -41,36 +45,66 @@ loadJSON('/Rules/rules.json').then(function (response) {
     };
 
     var idMap = new Map();
+    var defaultMap = new Map();
+
+    // Create map of default rules
+    for (var r of rules) {
+        if (r["default"] === true) {
+            if (defaultMap.get(r["action"]) === undefined) {
+                // First time for this object id
+                defaultMap.set(r["action"], r)
+            }
+        }
+    }
 
     // Create the map of the rules
     for (var r of rules) {
-        var id = r["object"];
+        var id = r["object"]
+        var action = r["action"]
 
-        if (document.getElementById(id) === null)
-            id = r["subject"]
+        if (r["default"] === true)
+            continue
 
         if (idMap.get(id) != undefined) {
             var ruleMap = idMap.get(id);
 
-            if (ruleMap.get(r["action"]) != undefined) {
+            if (ruleMap.get(action) != undefined) {
                 // This action is already present in this object
-                ruleMap.get(r["action"]).push(r);
+                ruleMap.get(action).push(r);
             } else {
                 // First time for this action with this object
-                ruleMap.set(r["action"], [r])
+                ruleMap.set(action, [r])
             }
         } else {
             // First time for this object id
-            idMap.set(id, new Map().set(r["action"], [r]))
+            idMap.set(id, new Map().set(action, [r]))
+        }
+
+
+        if (r["then"]["action"] != undefined) {
+            id = r["then"]["object"]
+            action = r["then"]["action"]
+            r2 = defaultMap.get(r["then"]["action"])
+
+            // TODO: da sistemare
+            if (idMap.get(id) != undefined) {
+                var ruleMap = idMap.get(id);
+    
+                if (ruleMap.get(action) === undefined) {
+                    // First time for this action with this object
+                    ruleMap.set(action, [r2])
+                }
+            } else {
+                // First time for this object id
+                idMap.set(id, new Map().set(action, [r2]))
+            }
         }
     }
 
     // Add events listeners
     for (let [id, actions] of idMap) {
         for (let [action, rules] of actions) {
-            document.getElementById(id).addEventListener(action, function (event) {
-                console.log("asd")
-
+            document.getElementById(id).addEventListener(removeSpaces(action), function (event) {
                 // copy the object (so we don't lose the old state)
                 var object = event.target.cloneNode(true);
                 object.object3D = event.target.object3D.clone();        // deepclone of threejs object
@@ -101,13 +135,13 @@ loadJSON('/Rules/rules.json').then(function (response) {
                         attribute = attribute.replace("}", "");
 
                         attribute = eval(attribute);
-                    } else{
+                    } else {
                         attribute = object.getAttribute(attribute);
                     }
 
                     // then
                     var objectThen = r["then"]["object"];
-                    var attributeThen = r["then"]["attribute"];
+                    var attributeThen = r["then"]["attribute"] || r["then"]["action"];
                     var valueThen = r["then"]["value"];
 
 
@@ -127,7 +161,7 @@ loadJSON('/Rules/rules.json').then(function (response) {
                         var before = attributeThen.match(/(\w+?)\.\w+/)[1];
                         var after = attributeThen.match(/\w+\.(\w+?)/)[1];
 
-                        var currentValue = {... document.getElementById(objectThen).getAttribute(before)};
+                        var currentValue = { ...document.getElementById(objectThen).getAttribute(before) };
                         currentValue[after] = valueThen;
                         valueThen = currentValue;
                         attributeThen = before;
@@ -137,7 +171,13 @@ loadJSON('/Rules/rules.json').then(function (response) {
 
                     // execute "then" if the "condition" is true or if no condition is defined
                     if (Object.entries(r["if"]).length === 0 || operators[condition](attribute, value)) {
-                        modifiedObject.setAttribute(attributeThen, valueThen);
+                        if (r["default"] == true) {
+                            // Fallback to map custom actions rules (e.g. change color to) to real events
+                            modifiedObject.setAttribute(attributeThen, modifiedObject.getAttribute(removeSpaces(action)))
+                        } else {
+                            modifiedObject.setAttribute(removeSpaces(attributeThen), valueThen);
+                            modifiedObject.dispatchEvent(new Event(removeSpaces(attributeThen)))
+                        }
                     }
                 }
             });
