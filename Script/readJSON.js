940f23fa3a8d019d16717a89e06a24c5a2041f45
Emanuele Concas <conema@outlook.com>
2020-08-16 15:00:52 +0200

    new map, multi condition and multi then

--- Script/readJSON.js
+++ Script/readJSON.js
@@ -52,6 +52,8 @@ loadJSON('/Rules/rules.json').then(function (response) {
     synonyms.set("on", 1)
     synonyms.set("is", "==")
     synonyms.set("is not", "!=")
+    synonyms.set("closed", "true")
+    synonyms.set("open", "false")
 
     var idMap = new Map();
     var defaultMap = new Map();
@@ -90,22 +92,24 @@ loadJSON('/Rules/rules.json').then(function (response) {
         }
 
 
-        if (r["then"]["action"] != undefined) {
-            id = r["then"]["object"]
-            action = r["then"]["action"]
-            r2 = defaultMap.get(r["then"]["action"])
+        for (var then of r["then"]) {
+            if (then["action"] != undefined) {
+                id = then["subject"]
+                action = then["action"]
+                r2 = defaultMap.get(then["action"])
 
-            // TODO: da sistemare
-            if (idMap.get(id) != undefined) {
-                var ruleMap = idMap.get(id);
+                // TODO: da sistemare
+                if (idMap.get(id) != undefined) {
+                    var ruleMap = idMap.get(id);
 
-                if (ruleMap.get(action) === undefined) {
-                    // First time for this action with this object
-                    ruleMap.set(action, [r2])
+                    if (ruleMap.get(action) === undefined) {
+                        // First time for this action with this object
+                        ruleMap.set(action, [r2])
+                    }
+                } else {
+                    // First time for this object id
+                    idMap.set(id, new Map().set(action, [r2]))
                 }
-            } else {
-                // First time for this object id
-                idMap.set(id, new Map().set(action, [r2]))
             }
         }
     }
@@ -113,6 +117,7 @@ loadJSON('/Rules/rules.json').then(function (response) {
     // Add events listeners
     for (let [id, actions] of idMap) {
         for (let [action, rules] of actions) {
+
             document.getElementById(id).addEventListener(removeSpaces(action), function (event) {
                 // copy the object (so we don't lose the old state)
                 var object = event.target.cloneNode(true);
@@ -122,101 +127,118 @@ loadJSON('/Rules/rules.json').then(function (response) {
                 var oldStates = new Map();
 
                 for (var r of rules) {
-                    //  condition
-                    var resultCondition = false;
-                    var objectCondition = r["if"]["object"];
-                    var attribute = r["if"]["attribute"];
-                    var condition = r["if"]["condition"];
-                    var value = r["if"]["value"];
+                    var conditionEvaluated = true
+                    if (r["if"] != undefined) {
+                        for (var ifr of r["if"]) {
+                            //  if
+                            var resultCondition = false;
+                            var objectCondition = ifr["object"];
+                            var attribute = ifr["attribute"];
+                            var condition = ifr["condition"];
+                            var value = ifr["value"];
 
-                    // Check synonyms
-                    if (synonyms.get(condition) != undefined) {
-                        condition = synonyms.get(condition)
-                    }
-                    
-                    if (synonyms.get(value) != undefined) {
-                        value = synonyms.get(value)
-                    }
-
-                    // Evaluation condition
-                    if (typeof (value) == "string" && value.includes(".")) {
-                        value = value.replace(/([A-z1-9]+)\.([A-z1-9]+)\.([A-z1-9]+)/g, "document.getElementById(\"$1\").getAttribute(\"$2\").$3");
-                        value = value.replace(/[^\.]\b([A-z]+)\.([A-z]+)[\s\}\+\*\-\\\%\&\|]/g, "document.getElementById(objectCondition).getAttribute(\"$1\").$2");
-
-                        value = value.replace("{", "");
-                        value = value.replace("}", "");
-
-                        value = eval(value);
-                    }
-
-                    if (typeof (attribute) == "string" && attribute.includes(".")) {
-                        attribute = attribute.replace(/([A-z]|\.)+/g, "document.getElementById(objectCondition).getAttribute(\"$&\")");
-                        attribute = attribute.replace(/\(\"(\w+)(\.\w)\"\)/g, "(\"$1\")$2");
-                        attribute = attribute.replace("{", "");
-                        attribute = attribute.replace("}", "");
-
-                        attribute = eval(attribute);
-                    } else {
-                        if (objectCondition != undefined) {
-                            // if an object is defined in the condition
-
-
-                            if (oldStates.get(objectCondition) === undefined) {
-                                // Register the original state
-                                state = document.getElementById(objectCondition).cloneNode(true);
-                                state.object3D = document.getElementById(objectCondition).object3D.clone();
-                                oldStates.set(objectCondition, state)
+                            // Check synonyms
+                            if (synonyms.get(condition) != undefined) {
+                                condition = synonyms.get(condition)
                             }
 
-                            attribute = oldStates.get(objectCondition).getAttribute(attribute)
-                        } else {
-                            // if no object is defined in the condition, take the object of the when
-                            attribute = object.getAttribute(attribute)
+                            if (synonyms.get(value) != undefined) {
+                                value = synonyms.get(value)
+                            }
+
+                            // Evaluation condition
+                            if (typeof (value) == "string" && value.includes(".")) {
+                                value = value.replace(/([A-z1-9]+)\.([A-z1-9]+)\.([A-z1-9]+)/g, "document.getElementById(\"$1\").getAttribute(\"$2\").$3");
+                                value = value.replace(/[^\.]\b([A-z]+)\.([A-z]+)[\s\}\+\*\-\\\%\&\|]/g, "document.getElementById(objectCondition).getAttribute(\"$1\").$2");
+
+                                value = value.replace("{", "");
+                                value = value.replace("}", "");
+
+                                value = eval(value);
+                            }
+
+                            if (typeof (attribute) == "string" && attribute.includes(".")) {
+                                attribute = attribute.replace(/([A-z]|\.)+/g, "document.getElementById(objectCondition).getAttribute(\"$&\")");
+                                attribute = attribute.replace(/\(\"(\w+)(\.\w)\"\)/g, "(\"$1\")$2");
+                                attribute = attribute.replace("{", "");
+                                attribute = attribute.replace("}", "");
+
+                                attribute = eval(attribute);
+                            } else {
+                                if (objectCondition != undefined) {
+                                    // if an object is defined in the condition
+
+
+                                    if (oldStates.get(objectCondition) === undefined) {
+                                        // Register the original state
+                                        state = document.getElementById(objectCondition).cloneNode(true);
+                                        state.object3D = document.getElementById(objectCondition).object3D.clone();
+                                        oldStates.set(objectCondition, state)
+                                    }
+
+                                    attribute = oldStates.get(objectCondition).getAttribute(attribute)
+                                } else {
+                                    // if no object is defined in the condition, take the object of the when
+                                    attribute = object.getAttribute(attribute)
+                                }
+                            }
+
+                            conditionEvaluated = conditionEvaluated && operators[condition](attribute, value)
                         }
                     }
 
-                    // then
-                    var objectThen = r["then"]["object"];
-                    var attributeThen = r["then"]["attribute"] || r["then"]["action"];
-                    var valueThen = r["then"]["value"];
+                    for (var then of r["then"]) {
+                        // then
+                        var subjectThen = then["subject"];
+                        var attributeThen = then["attribute"] || then["action"];
+                        var valueThen = then["value"];
 
-                    // Check synonyms
-                    if (synonyms.get(valueThen) != undefined) {
-                        valueThen = synonyms.get(valueThen)
-                    }
+                        var modifiedObject = document.getElementById(subjectThen) || event.target;
 
-                    // Evaluation value
-                    if (typeof (valueThen) == "string" && valueThen.charAt(0) == "{") {
-                        valueThen = valueThen.replace(/([A-z1-9]+)\.([A-z1-9]+)\.([A-z1-9]+)/g, "document.getElementById(\"$1\").getAttribute(\"$2\").$3");
-                        valueThen = valueThen.replace(/[^\.]\b([A-z]+)\.([A-z]+)[\s\}\+\*\-\\\%\&\|]/g, "document.getElementById(objectThen).getAttribute(\"$1\").$2");
+                        if (r["default"]) {
+                            valueThen = modifiedObject.getAttribute(removeSpaces(action))
+                        }
 
-                        valueThen = valueThen.replace("{", "");
-                        valueThen = valueThen.replace("}", "");
+                        // Check synonyms
+                        if (synonyms.get(valueThen) != undefined) {
+                            valueThen = synonyms.get(valueThen)
+                        }
 
-                        valueThen = eval(valueThen);
-                    }
+                        // Evaluation value
+                        if (typeof (valueThen) == "string" && valueThen.charAt(0) == "{") {
+                            valueThen = valueThen.replace(/([A-z1-9]+)\.([A-z1-9]+)\.([A-z1-9]+)/g, "document.getElementById(\"$1\").getAttribute(\"$2\").$3");
+                            valueThen = valueThen.replace(/[^\.]\b([A-z]+)\.([A-z]+)[\s\}\+\*\-\\\%\&\|]/g, "document.getElementById(subjectThen).getAttribute(\"$1\").$2");
 
-                    // Evaluation attribute
-                    if (attributeThen.includes(".")) {
-                        var before = attributeThen.match(/(\w+?)\.\w+/)[1];
-                        var after = attributeThen.match(/\w+\.(\w+?)/)[1];
+                            valueThen = valueThen.replace("{", "");
+                            valueThen = valueThen.replace("}", "");
 
-                        var currentValue = { ...document.getElementById(objectThen).getAttribute(before) };
-                        currentValue[after] = valueThen;
-                        valueThen = currentValue;
-                        attributeThen = before;
-                    }
+                            valueThen = eval(valueThen);
+                        }
 
-                    var modifiedObject = document.getElementById(objectThen) || event.target;
+                        // Evaluation attribute
+                        if (attributeThen.includes(".")) {
+                            var before = attributeThen.match(/(\w+?)\.\w+/)[1];
+                            var after = attributeThen.match(/\w+\.(\w+?)/)[1];
 
-                    // execute "then" if the "condition" is true or if no condition is defined
-                    if (Object.entries(r["if"]).length === 0 || operators[condition](attribute, value)) {
-                        if (r["default"] == true) {
-                            // Fallback to map custom actions rules (e.g. change color to) to real events
-                            modifiedObject.setAttribute(attributeThen, modifiedObject.getAttribute(removeSpaces(action)))
-                        } else {
+                            if (r["default"]) {
+                                var currentValue = { ...event.target.getAttribute(before) }
+                            } else {
+                                var currentValue = { ...document.getElementById(subjectThen).getAttribute(before) }
+                            }
+
+                            // Create position = {x, y, z}
+                            currentValue[after] = valueThen;
+                            valueThen = currentValue;
+                            attributeThen = before;
+                        }
+
+                        // Execute "then" if the conditions are true or if no condition is defined
+                        if (conditionEvaluated) {
                             modifiedObject.setAttribute(removeSpaces(attributeThen), valueThen);
-                            modifiedObject.dispatchEvent(new Event(removeSpaces(attributeThen)))
+                            if (!r["default"]) {
+                                // Dispach the event only if it isn't a default rules
+                                modifiedObject.dispatchEvent(new Event(removeSpaces(attributeThen)))
+                            }
                         }
                     }
                 }
